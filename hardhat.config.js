f3ee9d8e5ca31a35c3b376050cc793684c8a09ff
Anton Henningsson <anton_9408@hotmail.com>
2023-09-28 11:40:17 +0200

    create deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
-
+const { API_URL, PRIVATE_KEY } = process.env;
 
 
 module.exports = {
   solidity: "0.8.19",
+  defaultNetwork: 'sepolia',
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
